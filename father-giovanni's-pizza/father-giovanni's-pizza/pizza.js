4e4917460e7912a111525a7a4d908e59c8506713
phoebec0220 <94497454+phoebec0220@users.noreply.github.com>
2021-12-10 08:44:32 -0800

    Delete father-giovanni's-pizza/father-giovanni's-pizza directory

--- father-giovanni's-pizza/father-giovanni's-pizza/pizza.js
+++
@@ -1,8 +0,0 @@
-function openSide() {
-  document.getElementById("sidePop").style.width = "250px";
-  document.getElementById("inactive").style.width = "100%";
-}
-function closeSide() {
-  document.getElementById("sidePop").style.width = "0";
-  document.getElementById("inactive").style.width = "0";
-}
