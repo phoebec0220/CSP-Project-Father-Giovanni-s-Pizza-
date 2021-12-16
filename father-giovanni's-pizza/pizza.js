efa4364240d9444856a2556c9c3e695763df61e0
ruchadamle <95243352+ruchadamle@users.noreply.github.com>
2021-12-15 22:46:10 -0800

    Update pizza.js
    testing getting rid of the event listener

--- father-giovanni's-pizza/pizza.js
+++ father-giovanni's-pizza/pizza.js
@@ -54,10 +54,6 @@ const NAME_REQUIRED = "Please enter your name";
 const EMAIL_REQUIRED = "Please enter your email";
 const EMAIL_INVALID = "Please enter a correct email address format";
 
-form.addEventListener("submit", function (event) {
-  // stop form submission
-  event.preventDefault();
-
   // validate the form
   let nameValid = hasValue(form.elements["name"], NAME_REQUIRED);
   let emailValid = validateEmail(
