f12a4984c0e4ab5377697424c002600429ae99d3
phoebec0220 <94497454+phoebec0220@users.noreply.github.com>
2021-12-18 13:36:33 -0800

    Delete father-giovanni's-pizza/father-giovanni's-pizza directory

--- father-giovanni's-pizza/father-giovanni's-pizza/pizza.php
+++
@@ -1,7 +0,0 @@
-require_once "login.php";
-include_once "formResponse.html";
-$conn = myspli_connect($host, $user, $pass, $db, $port);
-if($conn->connect_error) die ($conn->connect_error);
-$query = "insert into respTable
-(fullName, email, msg)values(" . $_Post[name] . "," . $_Post[email] . "," . $_Post[message] . ");" ;
-mysqli_query($conn, $query); 
--- father-giovanni's-pizza/father-giovanni's-pizza/response.php
+++
@@ -1,7 +0,0 @@
-require_once "login.php";
-include_once "response.html";
-$conn = myspli_connect($host, $user, $pass, $db, $port);
-if($conn->connect_error) die ($conn->connect_error);
-while ($row = mysqli_fetch_assoc($result)) { 
-    echo  $row["fullName"] . "," . $row["email"] . ":" . $row["msg"];
-}
